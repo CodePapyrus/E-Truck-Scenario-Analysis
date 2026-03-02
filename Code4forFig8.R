f38cca182f099ff2aa3401b39d9147161c82cd8a
CodePapyrus <36542781+CodePapyrus@users.noreply.github.com>
2026-03-02 22:59:29 +0800

    Update Code4forFig8.R

--- Code4forFig8.R
+++ Code4forFig8.R
@@ -1,5 +1,4 @@
-#绘制不同外包比例，对成本的影响（同样分市区和郊区来进行比较）
-#1图
+# Plot the impact of different outsourcing ratios on transportation costs (comparison between suburbs and downtown)
 par(mfrow = c(1,1))
 draw_data <- as.matrix(data.frame(CP_A=c(604507.0975,436006.7708),
                                   CP_B=c(640806.9921,462106.6657),
@@ -14,23 +13,17 @@ draw_data <- as.matrix(data.frame(CP_A=c(604507.0975,436006.7708),
                                   CP_K=c(967200,697600)))
 colnames(draw_data) <- c("0","10","20","30","40","50","60","70","80","90","100")  
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-a <- barplot(draw_data,main="Figure 8 The impact of outsourcing ratio on transportation costs",xlab="Transportation costs (yuan)",ylab="Outsourcing ratio (%)",col=colors,horiz = TRUE,beside= TRUE)
+a <- barplot(draw_data, main="Figure 8 The impact of outsourcing ratio on transportation costs", 
+             xlab="Transportation costs (yuan)", ylab="Outsourcing ratio (%)", 
+             col=colors, horiz=TRUE, beside=TRUE)
 
 par(bg="#F2EBE5")
 
-
-
-
 legend("bottomright",
-       legend = c("Suburbs","Downtown"),
-       fill = c("#D5E490","#831A21"),
+       legend=c("Suburbs","Downtown"),
+       fill=c("#D5E490","#831A21"),
        bty="n",
        cex=1)
-
-
-
