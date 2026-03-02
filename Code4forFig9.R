36eaa284a50303484f8bbaf00c539728158edf1d
CodePapyrus <36542781+CodePapyrus@users.noreply.github.com>
2026-03-02 23:03:08 +0800

    Update Code4forFig9.R

--- Code4forFig9.R
+++ Code4forFig9.R
@@ -1,55 +1,39 @@
-#车辆在不同载重限制下，对运输结果的影响。
+# Impact of different vehicle weight limits on transportation results
 
 par(mfrow = c(3,1))
 
-
-#1图
+# Figure 1
 draw_data <- as.matrix(data.frame(weight_limitations=c(604507.0975,436006.7708)))
-#rownames <- c("")
+# rownames <- c("")
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-a <- barplot(draw_data,main="Figure 9(a) BETs' Weight Limitations Comparison",xlab="TC (yuan)",ylab = "2.5 (t)",col=colors,horiz = TRUE,beside= TRUE
-             )
+a <- barplot(draw_data, main="Figure 9(a) BETs' Weight Limitations Comparison", 
+             xlab="TC (yuan)", ylab="2.5 (t)", col=colors, horiz=TRUE, beside=TRUE)
 
 par(bg="#F2EBE5")
 
-#legend("topright",
- #      legend = c("Suburbs","Downtown"),
-  #     fill = c("#D5E490","#831A21"),
-   #    bty="n",
-    #   cex=1)
+# legend("topright",
+#        legend = c("Suburbs","Downtown"),
+#        fill = c("#D5E490","#831A21"),
+#        bty="n",
+#        cex=1)
 
-#2图
+# Figure 2
 draw_data <- as.matrix(data.frame(weight_limitations=c(604507.0975,436006.7708)))
 rownames <- c("6t")
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-b <- barplot(draw_data,xlab="TC (yuan)",ylab = "6 (t)",col=colors,horiz = TRUE,beside= TRUE)
+b <- barplot(draw_data, xlab="TC (yuan)", ylab="6 (t)", col=colors, horiz=TRUE, beside=TRUE)
 
-
-
-
-#3图
+# Figure 3
 draw_data <- as.matrix(data.frame(weight_limitations=c(604507.0975,436006.7708)))
 rownames <- c("12t")
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-c <- barplot(draw_data,xlab="TC (yuan)",ylab = "12 (t)",col=colors,horiz = TRUE,beside= TRUE)
-
-
-
-
-
-
+c <- barplot(draw_data, xlab="TC (yuan)", ylab="12 (t)", col=colors, horiz=TRUE, beside=TRUE)
