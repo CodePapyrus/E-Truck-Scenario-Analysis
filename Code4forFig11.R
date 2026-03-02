f8e284669a82dd0ef7bbd1a0c6af9bf61944ea66
CodePapyrus <36542781+CodePapyrus@users.noreply.github.com>
2026-03-02 23:06:32 +0800

    Update Code4forFig11.R

--- Code4forFig11.R
+++ Code4forFig11.R
@@ -1,46 +1,41 @@
-#车辆在不同载重限制下，对运输结果的影响。
+# Impact of different failure time thresholds on transportation results
 par(mfrow = c(3,1))
 
-#1图
+# Figure 1
 draw_data <- as.matrix(data.frame(mins=c(604507.0975,436006.7708)))
-#rownames <- c("800","300","200")
+# rownames <- c("800","300","200")
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-a <- barplot(draw_data,main="Figure 11 Failure Rate Comparison",xlab="TC (yuan)",ylab="800 (mins)",xlim=c(0,15e+05),col=colors,horiz = TRUE,beside= TRUE)
+a <- barplot(draw_data, main="Figure 11 Failure Rate Comparison", 
+             xlab="TC (yuan)", ylab="800 (mins)", xlim=c(0,15e+05), 
+             col=colors, horiz=TRUE, beside=TRUE)
 
 par(bg="#F2EBE5")
 
-#legend("topright",
- #      legend = c("Suburbs","Downtown"),
-  #     fill = c("#D5E490","#831A21"),
-   #    bty="n",
-    #   cex=1)
+# legend("topright",
+#        legend = c("Suburbs","Downtown"),
+#        fill = c("#D5E490","#831A21"),
+#        bty="n",
+#        cex=1)
 
-#2图
-
+# Figure 2
 draw_data <- as.matrix(data.frame(mins=c(1529508.026,1119007.713)))
-#rownames <- c("800","300","200")
+# rownames <- c("800","300","200")
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-b <- barplot(draw_data,xlab="TC (yuan)",ylab="300 (mins)",xlim=c(0,15e+05),col=colors,horiz = TRUE,beside= TRUE)
+b <- barplot(draw_data, xlab="TC (yuan)", ylab="300 (mins)", xlim=c(0,15e+05), 
+             col=colors, horiz=TRUE, beside=TRUE)
 
-#3图
-
+# Figure 3
 draw_data <- as.matrix(data.frame(mins=c(248006.2066,178005.8749)))
-#rownames <- c("800","300","200")
+# rownames <- c("800","300","200")
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-c <- barplot(draw_data,xlab="TC (yuan)",ylab="2000 (mins)",xlim=c(0,15e+05),col=colors,horiz = TRUE,beside= TRUE)
-
+c <- barplot(draw_data, xlab="TC (yuan)", ylab="2000 (mins)", xlim=c(0,15e+05), 
+             col=colors, horiz=TRUE, beside=TRUE)
