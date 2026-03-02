dafc959022e43f716532733099f333ce9148669e
CodePapyrus <36542781+CodePapyrus@users.noreply.github.com>
2026-03-02 23:05:06 +0800

    Update Code4forFig10.R

--- Code4forFig10.R
+++ Code4forFig10.R
@@ -1,53 +1,39 @@
-#车辆在不同载重限制下，对运输结果的影响。
+# Impact of different vehicle mileage limits on transportation results
 
 par(mfrow = c(3,1))
 
-
-#1图
+# Figure 1
 draw_data <- as.matrix(data.frame(mileage_duration=c(604507.0975,436006.7708)))
 rownames <- c("EP")
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-a <- barplot(draw_data,main="Figure 10(a) BETs' Mileage Duration Comparison",xlab="TC (yuan)",ylab="300 (KM)",col=colors,horiz = TRUE,beside= TRUE)
+a <- barplot(draw_data, main="Figure 10(a) BETs' Mileage Duration Comparison", 
+             xlab="TC (yuan)", ylab="300 (KM)", col=colors, horiz=TRUE, beside=TRUE)
 
 par(bg="#F2EBE5")
 
-#legend("topright",
- #      legend = c("Suburbs","Downtown"),
-  #     fill = c("#D5E490","#831A21"),
-   #    bty="n",
-    #   cex=1)
+# legend("topright",
+#        legend = c("Suburbs","Downtown"),
+#        fill = c("#D5E490","#831A21"),
+#        bty="n",
+#        cex=1)
 
-#2图
+# Figure 2
 draw_data <- as.matrix(data.frame(mileage_duration=c(604507.0975,436006.7708)))
 rownames <- c("CDP")
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-b <- barplot(draw_data,xlab="TC (yuan)",ylab = "400 (KM)",col=colors,horiz = TRUE,beside= TRUE)
+b <- barplot(draw_data, xlab="TC (yuan)", ylab="400 (KM)", col=colors, horiz=TRUE, beside=TRUE)
 
-
-
-
-#3图
+# Figure 3
 draw_data <- as.matrix(data.frame(mileage_duration=c(604507.0975,436006.7708)))
 rownames <- c("CP")
 
-
-
-#categories <- ("Suburbs","Urban") 
+# categories <- ("Suburbs","Urban") 
 colors <- c("#D5E490","#831A21")
 
-c <- barplot(draw_data,xlab="TC (yuan)",ylab = "500 (KM)",col=colors,horiz = TRUE,beside= TRUE)
-
-
-
-
-
+c <- barplot(draw_data, xlab="TC (yuan)", ylab="500 (KM)", col=colors, horiz=TRUE, beside=TRUE)
